a8d8d7b0c0dc3cb05c40723609f9e2f595866a3f
Hernan Di Tano <hditano@gmail.com>
2021-04-04 03:15:18 -0300

    calculator exercise finished

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,38 @@
-function add () {
-	
+function add(a, b) {
+  return a + b;
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+  return a - b;
 }
 
-function sum () {
-	
+function sum(array) {
+  let result = array.reduce((sum, current) => sum + current, 0);
+	return result;
 }
 
-function multiply () {
-	
+function multiply(array) {
+	let result = array.reduce((sum, current) => sum * current, 1);
+	return result;
 }
 
-function power() {
-	
+function power(base, power) {
+  return Math.pow(base, power);
 }
 
-function factorial() {
-	
+function factorial(a) {
+  let total = 1;
+  for (let i = 1; i <= a; i++) {
+    total *= i;
+  }
+  return total;
 }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
